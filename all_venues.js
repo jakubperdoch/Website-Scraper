75be20aa58a20db130f42c74aa5ade5350eac8d6
Jakub Perdoch <perdochjakub@gmail.com>
2024-10-26 01:05:32 +0200

    add: query config

--- all_venues.js
+++ all_venues.js
@@ -4,222 +4,229 @@ const color = require('colors');
 const { firefox } = require('playwright');
 const axios = require('axios');
 const {
- getVenuesByLocation,
- getEventsByVenueWithPagination,
- saveFileData,
+	getVenuesByLocation,
+	getEventsByVenueWithPagination,
+	saveFileData,
 } = require('./core/venues.js');
+const { json } = require('stream/consumers');
 
 const defaultHeaders = {
- 'User-Agent':
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
- 'Accept-Language': 'en-US,en;q=0.9',
- Accept:
-  'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
- 'Cache-Control': 'no-cache',
- Connection: 'keep-alive',
- 'Accept-Encoding': 'gzip, deflate, br',
+	'User-Agent':
+		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+	'Accept-Language': 'en-US,en;q=0.9',
+	Accept:
+		'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+	'Cache-Control': 'no-cache',
+	Connection: 'keep-alive',
+	'Accept-Encoding': 'gzip, deflate, br',
 };
 
 async function runScrape(
- locationIndex = 0,
- limitCountVenues = -1,
- showTop100 = true,
- searchInfoViewed = true,
- currentProxy = -1
+	locationIndex = 0,
+	limitCountVenues = -1,
+	showTop100 = true,
+	searchInfoViewed = true,
+	currentProxy = -1
 ) {
- try {
-  const [settings, jsonLocation] = await Promise.all([
-   fs.readFile('config/config.json', 'utf-8'),
-   fs.readFile('config/location.json', 'utf-8'),
-  ]);
-  const jsonConfig = JSON.parse(settings);
-  const locationConfig = JSON.parse(jsonLocation);
-  const currentLocation = locationConfig.locations[locationIndex];
+	try {
+		const [settings, jsonLocation] = await Promise.all([
+			fs.readFile('config/config.json', 'utf-8'),
+			fs.readFile('config/location.json', 'utf-8'),
+		]);
+		const jsonConfig = JSON.parse(settings);
+		const locationConfig = JSON.parse(jsonLocation);
+		const currentLocation = locationConfig.locations[locationIndex];
 
-  let selectedProxy = null;
-  let axiosInstance = axios.create({ headers: defaultHeaders });
-  let configBrowser = { headless: true };
-  if (currentProxy !== -1) {
-   try {
-    const proxyFile = JSON.parse(
-     await fs.readFile('config/proxies.json', 'utf-8')
-    );
-    selectedProxy = proxyFile[currentProxy];
-    axiosInstance = axios.create({
-     proxy: selectedProxy,
-     headers: defaultHeaders,
-    });
-    configBrowser.proxy = {
-     server: `${selectedProxy.protocol}://${selectedProxy.host}:${selectedProxy.port}`,
-     username: selectedProxy.auth.username,
-     password: selectedProxy.auth.password,
-    };
-   } catch (pe) {
-    console.error('Proxy configuration error:', pe);
-   }
-  }
+		let selectedProxy = null;
+		let axiosInstance = axios.create({ headers: defaultHeaders });
+		let configBrowser = { headless: true };
+		if (currentProxy !== -1) {
+			try {
+				const proxyFile = JSON.parse(
+					await fs.readFile('config/proxies.json', 'utf-8')
+				);
+				selectedProxy = proxyFile[currentProxy];
+				axiosInstance = axios.create({
+					proxy: selectedProxy,
+					headers: defaultHeaders,
+				});
+				configBrowser.proxy = {
+					server: `${selectedProxy.protocol}://${selectedProxy.host}:${selectedProxy.port}`,
+					username: selectedProxy.auth.username,
+					password: selectedProxy.auth.password,
+				};
+			} catch (pe) {
+				console.error('Proxy configuration error:', pe);
+			}
+		}
 
-  await getCurrentLocation(axiosInstance);
+		await getCurrentLocation(axiosInstance);
 
-  const browser = await firefox.launch(configBrowser);
-  const context = await browser.newContext({
-   userAgent:
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-   extraHTTPHeaders: defaultHeaders,
-  });
+		const browser = await firefox.launch(configBrowser);
+		const context = await browser.newContext({
+			userAgent:
+				'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+			extraHTTPHeaders: defaultHeaders,
+		});
 
-  const result = [];
-  const venuesResult = { venues: '', totalEvents: 0 };
+		const result = [];
+		const venuesResult = { venues: '', totalEvents: 0 };
 
-  const handleCleanup = async (exitCode = 0) => {
-   await saveFileData(
-    venuesResult,
-    `result_venues_${currentLocation.name}.json`
-   );
-   await saveFileData(
-    joinAllEvents(result, result.length),
-    `all_events_${currentLocation.name}.json`
-   );
-   await browser.close();
-   process.exit(exitCode);
-  };
+		const handleCleanup = async (exitCode = 0) => {
+			await saveFileData(
+				venuesResult,
+				`result_venues_${currentLocation.name}.json`
+			);
+			await saveFileData(
+				joinAllEvents(result, result.length),
+				`all_events_${currentLocation.name}.json`
+			);
+			await browser.close();
+			process.exit(exitCode);
+		};
 
-  process.on('uncaughtException', async (err) => {
-   console.error('Uncaught Exception:', err);
-   await handleCleanup(1);
-  });
+		process.on('uncaughtException', async (err) => {
+			console.error('Uncaught Exception:', err);
+			await handleCleanup(1);
+		});
 
-  process.on('SIGTSTP', async () => {
-   console.log('Process suspended by user.');
-   await handleCleanup();
-  });
+		process.on('SIGTSTP', async () => {
+			console.log('Process suspended by user.');
+			await handleCleanup();
+		});
 
-  process.on('SIGINT', async () => {
-   console.log('Killing process!');
-   await handleCleanup();
-  });
+		process.on('SIGINT', async () => {
+			console.log('Killing process!');
+			await handleCleanup();
+		});
 
-  logInfo(`${color.green('Getting list venues - ' + currentLocation.name)}`);
-  const listVenues = await getVenuesByLocation(
-   jsonConfig.events_by_location,
-   currentLocation,
-   0,
-   axiosInstance
-  );
+		logInfo(`${color.green('Getting list venues - ' + currentLocation.name)}`);
+		const listVenues = await getVenuesByLocation(
+			jsonConfig.events_by_location,
+			currentLocation,
+			0,
+			axiosInstance
+		);
 
-  if (listVenues) {
-   logInfo(`Total venues found: ${color.yellow(listVenues.length)}`);
-   venuesResult.venues = listVenues;
+		if (listVenues) {
+			logInfo(`Total venues found: ${color.yellow(listVenues.length)}`);
+			venuesResult.venues = listVenues;
 
-   const processedVenues = new Set();
+			const processedVenues = new Set();
 
-   const venuePromises = listVenues
-    .slice(0, limitCountVenues === -1 ? listVenues.length : limitCountVenues)
-    .map(async (venue) => {
-     if (processedVenues.has(venue.venueName)) {
-      return;
-     }
-     processedVenues.add(venue.venueName);
+			const venuePromises = listVenues
+				.slice(0, limitCountVenues === -1 ? listVenues.length : limitCountVenues)
+				.map(async (venue) => {
+					if (processedVenues.has(venue.venueName)) {
+						return;
+					}
+					processedVenues.add(venue.venueName);
 
-     logInfo(`Searching events in ${color.yellow(venue.venueName)}`);
+					logInfo(`Searching events in ${color.yellow(venue.venueName)}`);
 
-     const tempR = await getEventsByVenueWithPagination(
-      jsonConfig,
-      venue,
-      currentLocation.name,
-      context,
-      searchInfoViewed,
-      axiosInstance
-     );
+					const tempR = await getEventsByVenueWithPagination(
+						jsonConfig,
+						venue,
+						currentLocation.name,
+						context,
+						searchInfoViewed,
+						axiosInstance
+					);
 
-     if (tempR.length > 0) {
-      tempR.forEach((event) => {
-       event.venueName = venue.venueName;
-      });
-      result.push(...tempR);
-      venue.events = tempR;
-      await saveFileData(
-       venuesResult,
-       `result_venues_${currentLocation.name}.json`
-      );
-      await saveFileData(
-       joinAllEvents(result, result.length),
-       `all_events_${currentLocation.name}.json`
-      );
-     }
+					if (tempR.length > 0) {
+						tempR.forEach((event) => {
+							event.venueName = venue.venueName;
+						});
+						result.push(...tempR);
+						venue.events = tempR;
+						await saveFileData(
+							venuesResult,
+							`result_venues_${currentLocation.name}.json`
+						);
 
-     logInfo(
-      `Total events found for ${color.green(venue.venueName)}: ${color.yellow(tempR.length)}`
-     );
-    });
-   await Promise.all(venuePromises);
+						await saveFileData(result, `${currentLocation.name}.json`);
 
-   venuesResult.totalEvents = result.length;
-   logInfo(`***Total events ${color.green(result.length)}`);
+						await saveFileData(
+							joinAllEvents(result, result.length),
+							`all_events_${currentLocation.name}.json`
+						);
+					}
 
-   if (result.length > 0) {
-    const allEvents = showTop100
-     ? joinAllEvents(result).slice(0, 100)
-     : joinAllEvents(result);
-    const fileName = showTop100
-     ? `top100_events_${currentLocation.name}.json`
-     : `all_events_${currentLocation.name}.json`;
-    await saveFileData(allEvents, fileName);
-   } else {
-    console.log('No events found');
-   }
-  }
- } catch (fileError) {
-  console.error('File error:', fileError);
- }
+					logInfo(
+						`Total events found for ${color.green(venue.venueName)}: ${color.yellow(tempR.length)}`
+					);
+				});
+			await Promise.all(venuePromises);
+
+			venuesResult.totalEvents = result.length;
+			logInfo(`***Total events ${color.green(result.length)}`);
+
+			if (result.length > 0) {
+				const allEvents = showTop100
+					? joinAllEvents(result).slice(0, 100)
+					: joinAllEvents(result);
+				const fileName = showTop100
+					? `top100_events_${currentLocation.name}.json`
+					: `all_events_${currentLocation.name}.json`;
+				await saveFileData(allEvents, fileName);
+			} else {
+				console.log('No events found');
+			}
+		}
+	} catch (fileError) {
+		console.error('File error:', fileError);
+	}
 }
 
 function compareEvents(a, b) {
- const lastBuySortWeight = (event) =>
-  typeof event.lastBuy === 'object' ? 0 : 1;
- return lastBuySortWeight(a) - lastBuySortWeight(b);
+	const lastBuySortWeight = (event) =>
+		typeof event.lastBuy === 'object' ? 0 : 1;
+	return lastBuySortWeight(a) - lastBuySortWeight(b);
 }
 
 function joinAllEvents(result, venueCount) {
- const groupedEvents = {};
- result.forEach((event) => {
-  const artistName = event.name;
-  if (!groupedEvents[artistName]) {
-   groupedEvents[artistName] = [];
-  }
-  groupedEvents[artistName].push(event);
- });
+	const groupedEvents = {};
 
- const consolidatedEvents = [];
- for (const artist in groupedEvents) {
-  const artistEvents = groupedEvents[artist];
-  const uniqueUrls = new Set();
-  const uniqueEvents = artistEvents.filter((event) => {
-   if (uniqueUrls.has(event.url)) return false;
-   uniqueUrls.add(event.url);
-   return true;
-  });
+	result.forEach((event) => {
+		const artistName = event.name;
+		if (!groupedEvents[artistName]) {
+			groupedEvents[artistName] = [];
+		}
+		groupedEvents[artistName].push(event);
+	});
 
-  consolidatedEvents.push({
-   artist,
-   events: uniqueEvents.map((event) => ({
-    eventName: event.name,
-    url: event.url,
-    formattedDateWithoutYear: event.formattedDateWithoutYear,
-    lastBuy: event.lastBuy,
-    minPrice: event.eventDetail?.minPrice,
-    maxPrice: event.eventDetail?.maxPrice,
-    venueName: event.venueName,
-   })),
-  });
- }
+	const consolidatedEvents = [];
+	for (const artist in groupedEvents) {
+		const artistEvents = groupedEvents[artist];
+		const uniqueUrls = new Set();
+		const uniqueEvents = artistEvents.filter((event) => {
+			if (uniqueUrls.has(event.url)) return false;
+			uniqueUrls.add(event.url);
+			return true;
+		});
 
- return {
-  totalEvents: venueCount,
-  events: consolidatedEvents.sort((a, b) =>
-   compareEvents(a.events[0], b.events[0])
-  ),
- };
+		consolidatedEvents.push({
+			artist,
+			events: uniqueEvents.map((event) => ({
+				eventName: event.name,
+				url: event.url,
+				formattedDateWithoutYear: event.formattedDateWithoutYear,
+				lastBuy: event.lastBuy,
+				minPrice: event.eventDetail?.minPrice,
+				maxPrice: event.eventDetail?.maxPrice,
+				quantity: event.eventDetail?.quantity,
+				betterValueTickets: event.eventDetail?.betterValueTickets,
+				venueName: event.venueName,
+			})),
+		});
+	}
+
+	return {
+		totalEvents: venueCount,
+		events: consolidatedEvents.sort((a, b) =>
+			compareEvents(a.events[0], b.events[0])
+		),
+	};
 }
 
 module.exports = { runScrape };
--- core/venues.js
+++ core/venues.js
@@ -2,268 +2,278 @@ const axios = require('axios');
 const fs = require('fs');
 const color = require('colors');
 const {
- logInfo,
- logError,
- askUserForRetry,
- getCurrentDate,
+	logInfo,
+	logError,
+	askUserForRetry,
+	getCurrentDate,
 } = require('../utils.js');
 const { getLastBuy, getNumberByString } = require('../core/viagogo.js');
 
 const MAX_RETRIES = 5;
 
 async function getVenuesByLocation(customParams, currentLocation) {
- customParams.lat = btoa(currentLocation.lat);
- customParams.lon = btoa(currentLocation.long);
+	customParams.lat = btoa(currentLocation.lat);
+	customParams.lon = btoa(currentLocation.long);
 
- try {
-  const response = await axios.get('https://www.viagogo.com/explore', {
-   params: customParams,
-   headers: {
-    'User-Agent':
-     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-   },
-  });
-  return response.data.events || null;
- } catch (error) {
-  console.log('Error fetching venues:', error);
-  return null;
- }
+	try {
+		const response = await axios.get('https://www.viagogo.com/explore', {
+			params: customParams,
+			headers: {
+				'User-Agent':
+					'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+			},
+		});
+		return response.data.events || null;
+	} catch (error) {
+		return null;
+	}
 }
 
 async function getEventsByVenue(
- customParams,
- venue,
- locationName,
- axiosInstance
+	customParams,
+	venue,
+	locationName,
+	axiosInstance
 ) {
- console.log('Getting events - page: ' + customParams.pageIndex);
- customParams.venueId = venue.venueId;
+	console.log('Getting events - page: ' + customParams.pageIndex);
+	customParams.venueId = venue.venueId;
 
- const currentUrl = await generateUrlVenue(locationName, venue, axiosInstance);
- if (!currentUrl) return [];
+	const currentUrl = await generateUrlVenue(locationName, venue, axiosInstance);
+	if (!currentUrl) return [];
 
- const url = `${currentUrl}?pageIndex=${customParams.pageIndex}&method=GetFilteredEvents&venueId=${venue.venueId}&to=9999-12-31T23:59:59.999Z&nearbyGridRadius=50`;
+	const url = `${currentUrl}?pageIndex=${customParams.pageIndex}&method=GetFilteredEvents&venueId=${venue.venueId}&to=9999-12-31T23:59:59.999Z&nearbyGridRadius=50`;
 
- try {
-  const response = await axiosInstance.post(url);
-  return response.data;
- } catch (error) {
-  return [];
- }
+	try {
+		const response = await axiosInstance.post(url);
+		return response.data;
+	} catch (error) {
+		return [];
+	}
 }
 
 async function getEventsByVenueWithPagination(
- jsonConfig,
- venue,
- locationName,
- browser,
- searchInfoViewed,
- axiosInstance
+	jsonConfig,
+	venue,
+	locationName,
+	browser,
+	searchInfoViewed,
+	axiosInstance
 ) {
- const customParams = {
-  ...jsonConfig.venue_items,
-  venueId: venue.venueId,
-  pageIndex: 0,
- };
- const customResult = [];
+	const customParams = {
+		...jsonConfig.venue_items,
+		venueId: venue.venueId,
+		pageIndex: 0,
+	};
+	const customResult = [];
 
- while (true) {
-  const tempEvents = await getEventsByVenue(
-   customParams,
-   venue,
-   locationName,
-   axiosInstance
-  );
-  if (tempEvents && tempEvents.items && tempEvents.items.length > 0) {
-   logInfo(
-    `Events found on page ${customParams.pageIndex}: ${color.yellow(tempEvents.items.length)}`
-   );
-   const events = await getEventInfo(
-    tempEvents.items,
-    browser,
-    jsonConfig,
-    axiosInstance
-   );
-   customResult.push(...events);
-   customParams.pageIndex++;
-  } else {
-   console.log('No more events found, moving to next venue.');
-   break;
-  }
- }
- console.log('Total events found for venue:', customResult.length);
- return customResult;
+	while (true) {
+		const tempEvents = await getEventsByVenue(
+			customParams,
+			venue,
+			locationName,
+			axiosInstance
+		);
+		if (tempEvents && tempEvents.items && tempEvents.items.length > 0) {
+			logInfo(
+				`Events found on page ${customParams.pageIndex}: ${color.yellow(tempEvents.items.length)}`
+			);
+
+			const events = await getEventInfo(
+				tempEvents.items,
+				browser,
+				jsonConfig,
+				axiosInstance,
+				tempEvents.quantity,
+				tempEvents.betterValueTickets
+			);
+			customResult.push(...events);
+			customParams.pageIndex++;
+		} else {
+			console.log('No more events found, moving to next venue.');
+			break;
+		}
+	}
+	console.log('Total events found for venue:', customResult.length);
+	return customResult;
 }
 
-async function getEventInfo(listEvents, browser, jsonConfig, axiosInstance) {
- try {
-  return await Promise.all(
-   listEvents.map(async (event) => {
-    console.log('Processing event:', event.name);
-    const lastBuy = await getLastBuy(event.url, axiosInstance);
-    const eventUrl =
-     (await generateEventUrl(event.url, axiosInstance)) + '?qty=1';
+async function getEventInfo(
+	listEvents,
+	browser,
+	jsonConfig,
+	axiosInstance,
+	quantity,
+	betterValueTickets
+) {
+	try {
+		return await Promise.all(
+			listEvents.map(async (event) => {
+				console.log('Processing event:', event.name);
+				const lastBuy = await getLastBuy(event.url, axiosInstance);
+				const eventUrl =
+					(await generateEventUrl(event.url, axiosInstance)) + '?qty=0';
 
-    const viewed = searchInfoViewed
-     ? await listenInfoViewed(browser, eventUrl)
-     : 'ND';
+				const viewed = searchInfoViewed
+					? await listenInfoViewed(browser, eventUrl)
+					: 'ND';
 
-    const popups = await getPopupsEvent(event, jsonConfig.event_popups);
-    const eventDetail = await getDetailsEvent(
-     event,
-     jsonConfig.event_details,
-     axiosInstance
-    );
+				const popups = await getPopupsEvent(event, jsonConfig.event_popups);
+				const eventDetail = await getDetailsEvent(
+					event,
+					jsonConfig.event_details,
+					axiosInstance
+				);
 
-    return {
-     ...event,
-     lastBuy: lastBuy || 'ND',
-     numberRTimeLastBuy: 0,
-     timeSpread: getCurrentDate(),
-     viewed: viewed || 'ND',
-     numberViewed: getNumberByString(viewed || 'ND'),
-     popups,
-     eventDetail,
-     url: eventUrl,
-    };
-   })
-  );
- } catch (error) {
-  console.log('Error fetching event info:', error);
-  return [];
- }
+				return {
+					...event,
+					lastBuy: lastBuy || 'ND',
+					numberRTimeLastBuy: 0,
+					timeSpread: getCurrentDate(),
+					viewed: viewed || 'ND',
+					numberViewed: getNumberByString(viewed || 'ND'),
+					popups,
+					eventDetail,
+					url: eventUrl,
+				};
+			})
+		);
+	} catch (error) {
+		return [];
+	}
 }
 
 async function generateUrlVenue(locationName, venue, axiosInstance) {
- try {
-  const response = await axiosInstance.get(
-   `https://www.viagogo.com/_V-${venue.venueId}`
-  );
-  return response.request.res.responseUrl;
- } catch (error) {
-  const venueName = venue.venueName.replace(/ - |---|[\s,()"]/g, '-');
-  return `https://www.viagogo.com/${locationName}/${venueName}-Tickets/_V-${venue.venueId}`;
- }
+	try {
+		const response = await axiosInstance.get(
+			`https://www.viagogo.com/_V-${venue.venueId}`
+		);
+		return response.request.res.responseUrl;
+	} catch (error) {
+		const venueName = venue.venueName.replace(/ - |---|[\s,()"]/g, '-');
+		return `https://www.viagogo.com/${locationName}/${venueName}-Tickets/_V-${venue.venueId}`;
+	}
 }
 
 async function generateEventUrl(eventUrl, axiosInstance) {
- try {
-  const response = await axiosInstance.get(eventUrl);
-  return response.request.res.responseUrl;
- } catch (error) {
-  return eventUrl; // Fallback to constructed URL
- }
+	try {
+		const response = await axiosInstance.get(eventUrl);
+		return response.request.res.responseUrl;
+	} catch (error) {
+		return eventUrl; // Fallback to constructed URL
+	}
 }
 
 async function getDetailsEvent(event, customParams, axiosInstance) {
- console.log('Getting details for event:', event.name);
- if (!event.url) return null;
+	console.log('Getting details for event:', event.name);
+	if (!event.url) return null;
 
- try {
-  const response = await axiosInstance.post(event.url, customParams, {
-   headers: {
-    'User-Agent':
-     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-   },
-  });
-  const result = response.data;
-  delete result.items;
-  return result;
- } catch (error) {
-  console.log('Error fetching event details:', error);
-  return null;
- }
+	try {
+		const response = await axiosInstance.post(
+			event.url,
+			customParams,
+			{
+				headers: {
+					'User-Agent':
+						'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+				},
+			},
+			'?qty=0'
+		);
+
+		const result = response.data;
+		delete result.items;
+		return result;
+	} catch (error) {
+		return null;
+	}
 }
 
 async function getPopupsEvent(event, customParams) {
- console.log(`Getting popups for event [${event.name}]...`);
- if (!event.url) {
-  console.log('Invalid event URL.');
-  return null;
- }
+	console.log(`Getting popups for event [${event.name}]...`);
+	if (!event.url) {
+		console.log('Invalid event URL.');
+		return null;
+	}
 
- try {
-  const response = await axios.post(event.url, customParams, {
-   headers: {
-    'User-Agent':
-     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-   },
-  });
-  const resultData = response.data;
-  delete resultData.items;
-  return {
-   TicketsLeftMessage: resultData.TicketsLeftMessage,
-   LikelyToSellOutMessage: resultData.LikelyToSellOutMessage,
-   BestValueExplanationMessage: resultData.BestValueExplanationMessage,
-   LimitedQuantityRemainingMessage: resultData.LimitedQuantityRemainingMessage,
-   NumberOfGridResultsMessage: resultData.NumberOfGridResultsMessage,
-  };
- } catch (error) {
-  console.log('Error fetching popups event:', error);
-  return null;
- }
+	try {
+		const response = await axios.post(event.url, customParams, {
+			headers: {
+				'User-Agent':
+					'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+			},
+		});
+		const resultData = response.data;
+		delete resultData.items;
+		return {
+			TicketsLeftMessage: resultData.TicketsLeftMessage,
+			LikelyToSellOutMessage: resultData.LikelyToSellOutMessage,
+			BestValueExplanationMessage: resultData.BestValueExplanationMessage,
+			LimitedQuantityRemainingMessage: resultData.LimitedQuantityRemainingMessage,
+			NumberOfGridResultsMessage: resultData.NumberOfGridResultsMessage,
+		};
+	} catch (error) {
+		return null;
+	}
 }
 
 async function saveFileData(jsonData, fileName = 'result_venues.json') {
- const jsonString = JSON.stringify(jsonData, null, 2);
- const directory = './output/';
+	const jsonString = JSON.stringify(jsonData, null, 2);
+	const directory = './output/';
 
- if (!fs.existsSync(directory)) {
-  fs.mkdirSync(directory, { recursive: true });
- }
- const filePath = `${directory}${fileName}`;
+	if (!fs.existsSync(directory)) {
+		fs.mkdirSync(directory, { recursive: true });
+	}
+	const filePath = `${directory}${fileName}`;
 
- try {
-  await fs.promises.writeFile(filePath, jsonString, 'utf8');
-  logInfo('File saved successfully: ' + color.green(filePath));
- } catch (err) {
-  logError('Error saving file:', err);
- }
+	try {
+		await fs.promises.writeFile(filePath, jsonString, 'utf8');
+		logInfo('File saved successfully: ' + color.green(filePath));
+	} catch (err) {
+		logError('Error saving file:', err);
+	}
 }
 
 async function listenInfoViewed(browser, url) {
- const page = await browser.newPage();
- try {
-  await page.goto(url);
-  await page.waitForLoadState('networkidle');
-  const progressBarExists = await page.evaluate(
-   () => !!document.getElementById('progressbar')
-  );
+	const page = await browser.newPage();
+	try {
+		await page.goto(url);
+		await page.waitForLoadState('networkidle');
+		const progressBarExists = await page.evaluate(
+			() => !!document.getElementById('progressbar')
+		);
 
-  if (progressBarExists) {
-   await page.waitForFunction(
-    () => {
-     const progressBar = document.getElementById('progressbar');
-     return progressBar && progressBar.style.width === '100%';
-    },
-    { timeout: 10000 }
-   );
-  }
-  return await extractViewedText(page);
- } catch (error) {
-  console.log('Error in listenInfoViewed:', error);
-  return null;
- } finally {
-  await page.close();
- }
+		if (progressBarExists) {
+			await page.waitForFunction(
+				() => {
+					const progressBar = document.getElementById('progressbar');
+					return progressBar && progressBar.style.width === '100%';
+				},
+				{ timeout: 10000 }
+			);
+		}
+		return await extractViewedText(page);
+	} catch (error) {
+		return null;
+	} finally {
+		await page.close();
+	}
 }
 
 async function extractViewedText(page) {
- console.log('Retrieving information...');
- try {
-  const htmlContent = await page.content();
-  const match = htmlContent.match(/(\d+\speople\sviewed\sthis\sevent)/);
-  return match ? match[1] : '0';
- } catch (error) {
-  console.log('Error extracting viewed text:', error);
-  return '0';
- }
+	console.log('Retrieving information...');
+	try {
+		const htmlContent = await page.content();
+		const match = htmlContent.match(/(\d+\speople\sviewed\sthis\sevent)/);
+		return match ? match[1] : '0';
+	} catch (error) {
+		return '0';
+	}
 }
 
 module.exports = {
- getVenuesByLocation,
- getEventsByVenueWithPagination,
- saveFileData,
- getDetailsEvent,
- getPopupsEvent,
+	getVenuesByLocation,
+	getEventsByVenueWithPagination,
+	saveFileData,
+	getDetailsEvent,
+	getPopupsEvent,
 };
